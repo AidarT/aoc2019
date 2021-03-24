56f8894ccb49e350a3a1d1d28cee2f2d96d97858
Aidar Tukmeev <aidar.tu.n@gmail.com>
2021-03-24 13:26:34 +0300

    Day 10 Solvation. Part 1

--- aoc2019day10.py
+++ aoc2019day10.py
@@ -1,3 +1,6 @@
+from functools import reduce
+import math
+
 def sign(num):
     if num == 0:
         return 0
@@ -9,12 +12,19 @@ def checkUpDown(j, x, y, my_input, catches):
         if my_input[j][i] == '#':
             appendable = True
             for catch in catches[x, y]:
+                if i == catch[0] and x == i and ((y < catch[1] < j) or (y > catch[1] > j)):
+                    appendable = False
+                    break
                 x_dif, y_dif = catches[x, y][catch]
                 if x_dif != 0 and y_dif != 0:
+                    NOD = math.gcd(x_dif, y_dif)
+                    if NOD > 1:
+                        x_dif, y_dif = x_dif / NOD, y_dif / NOD
                     x_check = (x - i) % x_dif == 0 and sign(x - i) == sign(x_dif)
                     y_check = (y - j) % y_dif == 0 and sign(y - j) == sign(y_dif)
-                    line_check = (x - i) != x_dif and (y - j) != y_dif and (x - i) / x_dif == (y - j) / y_dif
-                    if ((x_check and y_check) or (x - i == y - j and x_dif == y_dif)) and line_check:
+                    line_check = (x - i) / x_dif == (y - j) / y_dif and sign(x - i) == sign(x_dif)
+                    diag_check = abs(x - i) == abs(y - j) and abs(catch[0] - i) == abs(catch[1] - j)
+                    if ((x_check and y_check) or diag_check) and line_check:
                         appendable = False
                         break
             if appendable:
@@ -25,14 +35,18 @@ with open('C:/Users/User/Documents/input.txt') as f:
     my_input = f.read().split('\n')
 f.close()
 
-catches = {}
+catches = {};
+errors = 0
 for y in range(0, len(my_input)):
     for x in range(0, len(my_input[0])):
         if my_input[y][x] == '#':
             catches[x, y] = {}
             if x + 1 < len(my_input[0]):
-                if my_input[y].index('#', x + 1) > 0:
-                    catches[x, y][my_input[y].index('#', x + 1), y] = x - my_input[y].index('#', x + 1), 0
+                try:
+                    if my_input[y].index('#', x + 1) > 0:
+                        catches[x, y][my_input[y].index('#', x + 1), y] = x - my_input[y].index('#', x + 1), 0
+                except:
+                    errors += 1
             if x - 1 >= 0:
                 for i in range(x - 1, -1, -1):
                     if my_input[y][i] == '#':
@@ -44,7 +58,7 @@ for y in range(0, len(my_input)):
             if y + 1 < len(my_input):
                 for j in range(y + 1, len(my_input), 1):
                     checkUpDown(j, x, y, my_input, catches)
-part1 = 0
+part1 = reduce(lambda prev, cur: len(catches[cur]) if len(catches[cur]) > prev else prev, catches, 0)
 
 part2 = 0
 
