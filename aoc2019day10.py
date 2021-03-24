6ae0b00089d5af53dd2bc8e6c6388cde570c4aea
Aidar Tukmeev <aidar.tu.n@gmail.com>
2021-03-24 15:49:35 +0300

    Day 10 Solvation

--- aoc2019day10.py
+++ aoc2019day10.py
@@ -8,28 +8,28 @@ def sign(num):
     return -1 if num < 0 else 1
 
 
-def checkUpDown(j, x, y, my_input, catches):
-    for i in range(0, len(my_input[y]), 1):
+def checkUpDown(j, x0, y0, my_input, catches):
+    for i in range(0, len(my_input[y0]), 1):
         if my_input[j][i] == '#':
             appendable = True
-            for catch in catches[x, y]:
-                if i == catch[0] and x == i and ((y < catch[1] < j) or (y > catch[1] > j)):
+            for x, y in catches[x0, y0]:
+                if i == x and x0 == i and ((y0 < y < j) or (y0 > y > j)):
                     appendable = False
                     break
-                x_dif, y_dif = catches[x, y][catch]
+                x_dif, y_dif = catches[x0, y0][x, y]
                 if x_dif != 0 and y_dif != 0:
                     NOD = math.gcd(x_dif, y_dif)
                     if NOD > 1:
                         x_dif, y_dif = x_dif / NOD, y_dif / NOD
-                    x_check = (x - i) % x_dif == 0 and sign(x - i) == sign(x_dif)
-                    y_check = (y - j) % y_dif == 0 and sign(y - j) == sign(y_dif)
-                    line_check = (x - i) / x_dif == (y - j) / y_dif and sign(x - i) == sign(x_dif)
-                    diag_check = abs(x - i) == abs(y - j) and abs(catch[0] - i) == abs(catch[1] - j)
+                    x_check = (x0 - i) % x_dif == 0 and sign(x0 - i) == sign(x_dif)
+                    y_check = (y0 - j) % y_dif == 0 and sign(y0 - j) == sign(y_dif)
+                    line_check = (x0 - i) / x_dif == (y0 - j) / y_dif and sign(x0 - i) == sign(x_dif)
+                    diag_check = abs(x0 - i) == abs(y0 - j) and abs(x - i) == abs(y - j)
                     if ((x_check and y_check) or diag_check) and line_check:
                         appendable = False
                         break
             if appendable:
-                catches[x, y][i, j] = x - i, y - j
+                catches[x0, y0][i, j] = x0 - i, y0 - j
 
 
 with open('C:/Users/User/Documents/input.txt') as f:
@@ -58,19 +58,16 @@ for y in range(0, len(my_input)):
             if y + 1 < len(my_input):
                 for j in range(y + 1, len(my_input), 1):
                     checkUpDown(j, x, y, my_input, catches)
-part1 = reduce(lambda prev, cur: len(catches[cur]) if len(catches[cur]) > prev else prev, catches, 0)
-
 x0, y0 = reduce(lambda prev, cur: cur if len(catches[cur]) > len(catches[prev]) else prev, catches)
+part1 = len(catches[x0, y0])
+
 part2 = catches[x0, y0]
 circle = {}
 for catch in part2:
     x_dif, y_dif = part2[catch]
     angle = math.atan2(-x_dif, y_dif)
     angle = angle + math.pi * 2 if angle < 0 else angle
-    if angle not in circle:
-        circle[angle] = catch
-    else:
-        circle[angle] = circle[angle], catch
+    circle[angle] = catch
 circle = {k: v for k, v in sorted(circle.items(), key=lambda item: item[0])}
 part2 = [v for k, v in circle.items()][199]
 part2 = part2[0] * 100 + part2[1]
