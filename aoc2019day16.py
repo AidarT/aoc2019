32bf3da2cd2a719f9af84863ac02d50152ce9c2e
Aidar Tukmeev <aidar.tu.n@gmail.com>
2021-04-07 17:55:32 +0300

    Day 16 Solvation. Part 1. Refactor

--- aoc2019day16.py
+++ aoc2019day16.py
@@ -1,24 +1,45 @@
 import re
+from functools import reduce
+
+
+def calcPattern(num, length):
+    pattern = [0, 1, 0, -1]
+    newPattern = []; iter = 0; ind = 0
+    for i in range (0, length + 1, 1):
+        if iter == num + 1:
+            iter = 0
+            ind = ind + 1 if ind + 1 < len(pattern) else 0
+        newPattern.append(pattern[ind])
+        iter += 1
+    newPattern.pop(0)
+    return newPattern
+
+# def calcFFTphase(input):
+#     newInput = input.copy()
+#     for ind in range(0, len(input), 1):
+#         newVal = 0; pat = 1; iter = 0; i = ind
+#         while i < len(input):
+#             newVal += input[i] * pat
+#             iter += 1
+#             if iter >= ind + 1:
+#                 iter = 0
+#                 pat *= -1
+#                 i = i + (ind + 1)
+#             i += 1
+#         newInput[ind] = abs(newVal) % 10
+#     return newInput
 
 
 def calcFFTphase(input):
     newInput = input.copy()
     for ind in range(0, len(input), 1):
-        newVal = 0; pat = 1; iter = 0; i = ind
-        while i < len(input):
-            newVal += input[i] * pat
-            iter += 1
-            if iter >= ind + 1:
-                iter = 0
-                pat *= -1
-                i = i + (ind + 1)
-            i += 1
+        pattern = calcPattern(ind, len(input))
+        newVal = reduce(lambda prev, cur: prev + input[cur[1]] * pattern[cur[0]], enumerate(input), 0)
         newInput[ind] = abs(newVal) % 10
     return newInput
 
-
 with open('C:/Users/User/Documents/input.txt') as f:
-    my_input = list(map(lambda a: int(a), re.findall(r'-?\d', f.read())))
+    my_input = list(map(lambda a: int(a), re.findall(r'\d', f.read())))
 f.close()
 
 orig_input = my_input.copy()
