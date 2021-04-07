8d5bd80feaa28893ee71118c6aed201689e87c82
Aidar Tukmeev <aidar.tu.n@gmail.com>
2021-04-07 17:55:47 +0300

    Day 17 Solvation. Part 1. Refactor

--- aoc2019day17.py
+++ aoc2019day17.py
@@ -76,46 +76,114 @@ class intcode:
                 break
 
 
+start_dirs = {"v": 1, "^": 2, ">": 3, "<": 4}
+dirs = {1: "D", 2: "U", 3: "R", 4: "L"}
 moves = {1: (0, 1), 2: (0, -1), 3: (1, 0), 4: (-1, 0)}
+backMoves = {1: 2, 2: 1, 3: 4, 4: 3}
+
+
+def nexMoveChoice(move, cur_coord, map):
+    new_coord = cur_coord[0] + moves[move][0], cur_coord[1] + moves[move][1]
+    if new_coord in map and (map[new_coord] == "#" or map[new_coord] == "O"):
+        return move
+    return 0
+
+
+def dirChoice(move, dir):
+    if (dir == 2 and move == 3) or (dir == 3 and move == 1) or (dir == 1 and move == 4) or (dir == 4 and move == 2):
+        return 3
+    elif (dir == 2 and move == 4) or (dir == 3 and move == 2) or (dir == 1 and move == 3) or (dir == 4 and move == 1):
+        return 4
+    return 0
+
+
+def fullPathCalc(move, cur_coord, map, amount, path, dir):
+    new_coord = cur_coord[0] + moves[move][0], cur_coord[1] + moves[move][1]
+    if map[new_coord] == "#" or map[new_coord] == "O":
+        while new_coord in map and (map[new_coord] == "#" or map[new_coord] == "O"):
+            cur_coord = new_coord
+            amount += 1
+            new_coord = cur_coord[0] + moves[move][0], cur_coord[1] + moves[move][1]
+        path.append(dirs[dirChoice(move, dir)])
+        path.append(amount)
+        amount = 0
+        old_dir = backMoves[move]
+        dir = move
+        for i in range(1, 5, 1):
+            if i != old_dir:
+                move = nexMoveChoice(i, cur_coord, map)
+                if move != 0:
+                    fullPathCalc(move, cur_coord, map, amount, path, dir)
+
 
 with open('C:/Users/User/Documents/input.txt') as f:
     my_input = list(map(lambda a: int(a), f.read().split(',')))
 f.close()
 
 my_input = {i: my_input[i] for i in range(0, len(my_input))}
-part1 = intcode(my_input.copy(), [])
+robot = intcode(my_input.copy(), [])
+while robot.run:
+        robot.output_calc()
 
-while part1.run:
-        part1.output_calc()
-
-amount = 0
-pic = list(map(lambda line: [char for char in line], ('').join([chr(i) for i in part1.output]).split('\n')))
+part1 = 0
+pic = list(map(lambda line: [char for char in line], ('').join([chr(i) for i in robot.output]).split('\n')))
 for j, line in enumerate(pic):
     for i, ch in enumerate(line):
         if j - 1 >= 0 and j + 1 < len(pic) and i - 1 >= 0 and i + 1 < len(line):
             if ch == '#' and pic[j][i - 1] == '#' and pic[j][i + 1] == '#' \
                     and pic[j - 1][i] == '#' and pic[j + 1][i] == '#':
                 pic[j][i] = 'O'
-                amount += j * i
+                part1 += j * i
     print(('').join(line))
 
+us_map = {(x, y): v for y, line in enumerate(pic) for x, v in enumerate(line)}
+start = reduce(lambda prev, cur: cur[0] if cur[1] == "^" or cur[1] == ">" or cur[1] == "<" or cur[1] == "v"
+    else prev, us_map.items(), (0, 0))
 
-coords = [k for k, v in sorted(map.items(), key=lambda item: item[0])]
-maxs = reduce(lambda prev, cur: [cur[0] if cur[0] > prev[0] else prev[0], cur[1] if cur[1] > prev[1] else prev[1]], coords, [0, 0])
-mins = reduce(lambda prev, cur: [cur[0] if cur[0] < prev[0] else prev[0], cur[1] if cur[1] < prev[1] else prev[1]], coords, [0, 0])
-
-pic = []
-for j in range(maxs[1], mins[1] - 1, -1):
-    pic.append([])
-    for i in range(mins[0], maxs[0] + 1, 1):
-        pic[len(pic) - 1].append('?' if (i, j) not in map else map[(i, j)])
-for line in pic:
-    print((' ').join(line).replace(',', ''))
-
-start = list(filter(lambda elem: elem[1] == 'X', map.items()))[0][0]
-oxygenMap = {start: 0}
-part2 = 0
+path = []
 for i in range(1, 5, 1):
-    part2 = oxygenCalc(i, start, map, 0, oxygenMap, part2)
+    start_move = nexMoveChoice(i, start, us_map)
+    if start_move != 0:
+        break
+fullPathCalc(start_move, start, us_map, 0, path, start_dirs[us_map[start]])
 
-print(str(amount) + " " + str(part2))
+
+def candidatesFound(to_find, ind, path, depth, candidates):
+    for i in range(ind + 2, len(path), 2):
+        to_find += ',' + (',').join(list(map(str, [path[i], path[i + 1]])))
+        amount = len((',').join(list(map(str, path))).split(to_find)) - 1
+        if amount > 1 and depth < 3 and i + 3 < len(path):
+            candidates[to_find] = amount
+            new_path = path_str.replace(to_find, '').replace(',,', ',')
+            if new_path[0] == ",":
+                new_path = new_path[1:]
+            to_find_new = (',').join(list(map(str, [path[i + 2], path[i + 3]])))
+            candidatesFound(to_find_new, ind, new_path.split(','), depth + 1, candidates)
+        else:
+            new_path = path_str
+            for j in range(0, 4, 1):
+                new_path = new_path.replace(to_find, '').replace(',,', ',')
+                if new_path[0] == ",":
+                    new_path = new_path[1:]
+
+
+candidates = {}
+path_str = (',').join(list(map(str, path)))
+# for j in range(0, len(path), 2):
+#     to_find = (',').join(list(map(str, [path[j], path[j + 1]])))
+#     for i in range(j + 2, len(path), 2):
+#         to_find += ',' + (',').join(list(map(str, [path[i], path[i + 1]])))
+#         amount = len(path_str.split(to_find)) - 1
+#         if amount > 1:
+#             candidates[to_find] = amount
+
+for j in range(0, len(path), 2):
+    to_find = (',').join(list(map(str, [path[j], path[j + 1]])))
+    candidates = candidatesFound(to_find, j, path, 0, candidates)
+
+my_input[0] = 2
+robot = intcode(my_input.copy(), [])
+
+part2 = 0
+
+print(str(part1) + " " + str(part2))
